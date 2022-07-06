7358842916b4370c8fe873e133e47f237f022875
Kirill Ivanov <kirdmiv@users.noreply.github.com>
2022-07-07 02:50:57 +0300

    init

---
+++ src/main/java/com/ivanov/kirill/EmployeeHandbook/EmployeeHandbookApplication.java
@@ -0,0 +1,13 @@
+package com.ivanov.kirill.EmployeeHandbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeHandbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeHandbookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ivanov/kirill/EmployeeHandbook/EmployeeHandbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivanov.kirill.EmployeeHandbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeHandbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
